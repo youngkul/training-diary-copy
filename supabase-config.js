90c99d346e6e35b5e3ae1b7b2a9d04600eaa1724
young kul yang <youngkulyang@youngui-MacBookPro.local>
2025-05-09 20:22:40 +0900

    Supabase 관련 파일 제거

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const SUPABASE_URL = "https://chyfkizuxorfgxdoqlxd.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoeWZraXp1eG9yZmd4ZG9xbHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNzI2MTAsImV4cCI6MjA2MTk0ODYxMH0.oGd7OTxHc6Gp8EYY_Akl8BwG6574zxvblPaYPiYviXA";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
